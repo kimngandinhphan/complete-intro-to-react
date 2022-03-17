388e936366b05980c0bc5f623a69fb6dc692f835
Kim Ngan Dinh Phan <kimngandinhphan@gmail.com>
2022-03-17 15:32:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom"
+import Details from "./Details"
+import SearchParams from "./SearchParams"
+import ThemeContext from "./ThemeContext"
+
+const App = () => {
+	const theme = useState("blue")
+	return (
+		<ThemeContext.Provider value={theme}>
+			<div>
+				<Router>
+					<header>
+						<Link to="/">
+							<h1>Adopt Me!</h1>
+						</Link>
+					</header>
+					<Switch>
+						<Route path="/details/:id">
+							<Details theme={theme} />
+						</Route>
+						<Route path="/">
+							<SearchParams />
+						</Route>
+					</Switch>
+				</Router>
+			</div>
+		</ThemeContext.Provider>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
